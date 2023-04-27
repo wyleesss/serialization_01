1796bc8789f3f05177b6bbd848c9b7b6043e1bc8
Koza_jp97@student.itstep.org <Koza_jp97@student.itstep.org>
2023-04-27 18:44:31 +0300

    Add project files.

---
+++ serialization_01/NeededClasses.cs
@@ -0,0 +1,150 @@
+﻿namespace NeededClasses
+{
+    [Serializable]
+    class Track
+    {
+        public ushort ID { get; }
+        public List<Classes> Classes { get; set; }
+
+        public Track(ushort ID)
+        {
+            this.ID = ID;
+            Classes = new();
+        }
+
+        public Track(ushort ID, List<Classes> work)
+        {
+            this.ID = ID;
+            Classes = work;
+        }
+
+        public override string ToString()
+        {
+            string res = $"[track]:\nID: {ID}\nclasses:\n";
+
+            foreach (var c in Classes)
+                res += c.ToString() + "\n";
+
+            return res;
+        }
+    }
+
+    [Serializable]
+    class Classes
+    {
+        public DateTime Time { get; set; }
+        public ushort TrackID { get; set; }
+        public string TrainerName { get; set; }
+        public ushort GroupID { get; set; }
+
+        public Classes(DateTime time, ushort trackID, string trainerName, ushort groupID)
+        {
+            Time = time;
+            TrackID = trackID;
+            TrainerName = trainerName;
+            GroupID = groupID;
+        }
+
+        public override string ToString()
+        {
+            return $"[classes]:\ntime: {Time}\ntrack ID: {TrackID}\ntrainer name: {TrainerName}\ngroup ID: {GroupID}\n";
+        }
+    }
+
+    [Serializable]
+    class Trainer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public uint HourSalary { get; set; }
+        public List<Classes> Classes { get; set; }
+
+        public string FullName => $"{Name} {Surname}";
+
+        public Trainer(string name, string surname, string phoneNumber, uint hourSalary)
+        {
+            Name = name;
+            Surname = surname;
+            PhoneNumber = phoneNumber;
+            HourSalary = hourSalary;
+            Classes = new();
+        }
+
+        public Trainer(string name, string surname, string phoneNumber, uint hourSalary, List<Classes> classes)
+        {
+            Name = name;
+            Surname = surname;
+            PhoneNumber = phoneNumber;
+            HourSalary = hourSalary;
+            Classes = classes;
+        }
+
+        public override string ToString()
+        {
+            string res = $"[trainer]:\nfull name: {FullName}\nphone number: {PhoneNumber}\nhour salary: {HourSalary}\nclasses:\n";
+
+            foreach (var c in Classes)
+                res += c.ToString() + "\n";
+
+            return res;
+        }
+    }
+
+    [Serializable]
+    class Group
+    {
+        public ushort ID { get; }
+        public List<Student> Students { get; set; }
+
+        public Group(ushort iD)
+        {
+            ID = iD;
+            Students = new();
+        }
+
+        public Group(ushort iD, List<Student> students)
+        {
+            ID = iD;
+            Students = students;
+        }
+
+        public override string ToString()
+        {
+            string res = $"[group]:\nID: {ID}\nstudents:\n";
+
+            foreach (var s in Students)
+                res += s.ToString() + "\n";
+
+            return res;
+        }
+    }
+
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ParentName { get; set; }
+        public string PhoneNumber { get; set; }
+        public ushort PayedClasses { get; set; }
+        public ushort GroupID { get; set; }
+
+        public string FullName => $"{Name} {Surname}";
+
+        public Student(string name, string surname, string parentName, string phoneNumber, ushort payedClasses, ushort groupID)
+        {
+            Name = name;
+            Surname = surname;
+            ParentName = parentName;
+            PhoneNumber = phoneNumber;
+            PayedClasses = payedClasses;
+            GroupID = groupID;
+        }
+
+        public override string ToString()
+        {
+            return $"[student]:\nfull name: {FullName}\nparent name: {ParentName}\nphone number: {PhoneNumber}\npayed classes: {PayedClasses}\ngroup ID: {GroupID}\n";
+        }
+    }
+}
---
+++ serialization_01/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+using NeededClasses;
+
+#pragma warning disable SYSLIB0011 // <-- вимикаємо помилку "SYSLIB0011" пов'язану з "BinaryFormatter.Serialize()"
+
+class Program
+{
+    static void Main()
+    {
+        Trainer trainer = new("Jack", "Greenlish", "+1023478654", 100);
+        Trainer? DeserializedTrainerObj = null;
+
+        Track track = new(11);
+        Track? DeserializedTrackObj = null;
+
+        Group group = new(127);
+        Group? DeserializedGroupObj = null;
+
+        Classes classes = new(DateTime.Now, track.ID, trainer.FullName, group.ID);
+        Student student = new("Alex", "Lovare", "Sabina Lovare", "+1023343411", 30, group.ID);
+
+        group.Students.Add(student);
+        track.Classes.Add(classes);
+        trainer.Classes.Add(classes);
+
+        BinaryFormatter binFormatter = new();
+
+        try
+        {
+            using (Stream fStream = File.Create("trainer.bin"))
+                binFormatter.Serialize(fStream, trainer);
+
+            using (Stream fStream = File.Create("track.bin"))
+                binFormatter.Serialize(fStream, track);
+
+            using (Stream fStream = File.Create("group.bin"))
+                binFormatter.Serialize(fStream, group);
+
+
+            using (Stream fStream = File.OpenRead("trainer.bin"))
+                DeserializedTrainerObj = (Trainer)binFormatter.Deserialize(fStream);
+
+            using (Stream fStream = File.OpenRead("track.bin"))
+                DeserializedTrackObj = (Track)binFormatter.Deserialize(fStream);
+
+            using (Stream fStream = File.OpenRead("group.bin"))
+                DeserializedGroupObj = (Group)binFormatter.Deserialize(fStream);
+
+            Console.WriteLine("--ORIGINAL:--\n");
+            Console.WriteLine(trainer);
+            Console.WriteLine(group);
+            Console.WriteLine(track);
+
+            Console.WriteLine("\n--DESERIALIZED:--\n");
+            Console.WriteLine(DeserializedTrainerObj);
+            Console.WriteLine(DeserializedGroupObj);
+            Console.WriteLine(DeserializedTrackObj);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
